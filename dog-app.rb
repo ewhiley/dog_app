d8fb9500b9bb2f9f6e8561d08ce4e9294cc29d19
Ruth_mci <45578199+ruthmci@users.noreply.github.com>
2019-03-06 09:50:13 +1100

    Revert "Ruth"

--- dog-app.rb
+++ dog-app.rb
@@ -13,7 +13,8 @@ class Dog
       @@counter += 1
       puts "I've been initialized!"
     end
-end
+  
+end   
 
 def register()
   puts "Dog name: "
@@ -28,34 +29,9 @@ def register()
   input_appointment = gets.strip
 
   registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
-  puts ("#{registered_dog.dog_name} is booked for their appointment at #{registered_dog.appointment}") 
- 
-  File.open("appts.csv", "a") do |dog| 
-    dog.puts ("\n#{registered_dog.appointment},#{registered_dog.dog_name}")
-  end
+  puts ("#{registered_dog.dog_name} has been registered") 
 end
 
-    print ("Select one of the followings options: 1- to Display Appts or 2- to Register a Dog ")
-    puts
-    option = gets().strip
-    case option 
-    when "1"
-      puts ("Today's Appointments:")
-        File.open("appts.csv").each do |line|
-             puts line
-            end 
-    when "2"
-     puts "#{register}"
-    else
-      puts("Error. Invalid option.")
-    end
+# dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
+register()
 
-
-    
-
-    # dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
-    # register()
-    
-
-
-   
