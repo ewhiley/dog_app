f1293d3386e1c3274b7db6655ba9779ef3f8e74c
Ruth <ruthmcilwaine@gmail.com>
2019-03-05 21:31:15 +1100

    update menu options

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Plan for App
 The aim of our project is to create a dog grooming app. It will assist a dog grooming business with registering new customers, and making appointments easily. We envisage the app to include at least one class, methods, case statements, a gem and testing methods. The dog class will be the initial blueprint for the process, creating dog objects. Register Dog will be an instance method that will allow the pet owner to register their dog in the system. Potentially within these methods, there is opportunity to create sub methods, which we need to confirm when we come to refactor our code. We see this as the MVP which is achievable in 2 days. 
 For the presentation, we want to do a walk through of the registration and create a new customer profile. 
 We see this project as being scalable and can been updated to include more services in the future, for example, breaking down the grooming appointment into several more specific services.
+
+[README explains the higher level structure of the app (e.g. why modules or functions were used or why separate files were utilized).]
--- dog-app.rb
+++ dog-app.rb
@@ -13,26 +13,44 @@ class Dog
       @@counter += 1
       puts "I've been initialized!"
     end
+end
 
-    def register()
-      puts "Dog name: "
-      input_name = gets.strip
-      puts "Dog breed: "
-      input_breed = gets.strip
-      puts "Owner name: "
-      input_owner_name = gets.strip
-      puts "Owner email: "
-      input_email = gets.strip
-      puts "Appointment: "
-      input_appointment = gets.strip
-    
-      registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
-      puts ("#{registered_dog.dog_name} has been registered") 
+def register()
+  puts "Dog name: "
+  input_name = gets.strip
+  puts "Dog breed: "
+  input_breed = gets.strip
+  puts "Owner name: "
+  input_owner_name = gets.strip
+  puts "Owner email: "
+  input_email = gets.strip
+  puts "Appointment: "
+  input_appointment = gets.strip
+
+  registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
+  puts ("#{registered_dog.dog_name} has been registered") 
+end
+
+    print ("Select one of the followings options: 1- to Display Appts or 2- to Register a Dog ")
+    puts
+    option = gets().strip
+    case option 
+    when "1"
+      puts ("Today's Appointments:")
+        File.open("appts.csv").each do |line|
+             puts line
+            end 
+    when "2"
+     puts "#{register}"
+    else
+      puts("Error. Invalid option.")
     end
-    
-    # dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
-    register()
-    
-  
 
-end   
+    
+
+    # dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
+    # register()
+    
+
+
+   
