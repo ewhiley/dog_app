34685281a8986655de923f77d8bae252b147596c
Ruth <ruthmcilwaine@gmail.com>
2019-03-05 16:04:07 +1100

    Pushing fixed code

--- dog-app.rb
+++ dog-app.rb
@@ -13,30 +13,26 @@ class Dog
       @@counter += 1
       puts "I've been initialized!"
     end
+
+    def register()
+      puts "Dog name: "
+      input_name = gets.strip
+      puts "Dog breed: "
+      input_breed = gets.strip
+      puts "Owner name: "
+      input_owner_name = gets.strip
+      puts "Owner email: "
+      input_email = gets.strip
+      puts "Appointment: "
+      input_appointment = gets.strip
+    
+      registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
+      puts ("#{registered_dog.dog_name} has been registered") 
+    end
+    
+    # dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
+    register()
+    
   
-<<<<<<< HEAD
 
 end   
-=======
-end   
-
-def register()
-  puts "Dog name: "
-  input_name = gets.strip
-  puts "Dog breed: "
-  input_breed = gets.strip
-  puts "Owner name: "
-  input_owner_name = gets.strip
-  puts "Owner email: "
-  input_email = gets.strip
-  puts "Appointment: "
-  input_appointment = gets.strip
-
-  registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
-  puts ("#{registered_dog.dog_name} has been registered") 
-end
-
-# dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
-register()
-
->>>>>>> 73e3ce5106ca769f72dabfa557f9b8d8c32ce1ff
