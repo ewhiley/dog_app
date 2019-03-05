f03435c5bc7fc7d402a8be2919e0eda99a0cbad2
ewhiley <31295147+ewhiley@users.noreply.github.com>
2019-03-05 14:17:17 +1100

    First commit of ruby file

---
+++ dog-app.rb
@@ -0,0 +1,18 @@
+class Dog
+
+@@counter = 0
+
+    attr_accessor(:dog_name, :breed, :owner_name, :owner_phone, :owner_email, :appointment)
+      
+    def initialize(dog_name, breed, owner_name, owner_phone, owner_email, appointment)
+      @dog_name = dog_name
+      @breed = breed
+      @owner_name = owner_name
+      @owner_phone = owner_phone
+      @owner_email = owner_email
+      @appointment = appointment
+        @@counter += 1
+      puts "I've been initialized!"
+    end
+  
+end   
