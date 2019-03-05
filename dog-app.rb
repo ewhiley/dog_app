f24c131c6b5fcc015f09c57d2651817dabefe024
ewhiley <ejwhiley29@gmail.com>
2019-03-05 15:45:53 +1100

    register method started

--- dog-app.rb
+++ dog-app.rb
@@ -2,20 +2,36 @@ class Dog
 
 @@counter = 0
 
-    attr_accessor(:dog_name, :breed, :owner_name, :owner_phone, :owner_email, :appointment)
+    attr_accessor(:dog_name, :breed, :owner_name, :owner_email, :appointment)
       
-    def initialize(dog_name, breed, owner_name, owner_phone, owner_email, appointment)
+    def initialize(dog_name, breed, owner_name, owner_email, appointment)
       @dog_name = dog_name
       @breed = breed
       @owner_name = owner_name
-      @owner_phone = owner_phone
       @owner_email = owner_email
       @appointment = appointment
-        @@counter += 1
+      @@counter += 1
       puts "I've been initialized!"
     end
   
 end   
 
 def register()
+  puts "Dog name: "
+  input_name = gets.strip
+  puts "Dog breed: "
+  input_breed = gets.strip
+  puts "Owner name: "
+  input_owner_name = gets.strip
+  puts "Owner email: "
+  input_email = gets.strip
+  puts "Appointment: "
+  input_appointment = gets.strip
+  
+  registered_dog = Dog.new(input_name, input_breed, input_owner_name, input_email, input_appointment)
+  puts ("#{registered_dog.dog_name} has been registered") 
 end
+
+# dog1 = Dog.new("ellie", "dacshund", "elizabeth", "0408319636", "ejwhiley29@gmail.com", "12 May 2019")
+register()
+
